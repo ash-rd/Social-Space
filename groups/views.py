3573d69faf08d0ebc4fa423c371bc5c28ae8316c
Ashton Fernandes <ashtonfernandess24@outlook.com>
2022-06-25 17:57:52 +0530

    Updated Files

---
+++ groups/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views import generic
+
+from .models import Group, GroupMember
+# Create your views here.
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+	model = Group
+	fields = ['name', 'description']
+	template_name = 'groups/group_form.html'
+
+class SingleGroup(generic.DetailView):
+	model = Group
+
+class ListGroup(generic.ListView):
+	model = Group
